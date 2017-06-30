5b27baf4f5c1882edc1cf77d5a33ab62e6fbd6cd
brent-edwards-online <brent@brentedwardsonline.com>
2017-06-30 10:43:40 +1000

    Initial Commit

---
+++ grade-scores/grade-scores/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grade_scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sup?");
+        }
+    }
+}
